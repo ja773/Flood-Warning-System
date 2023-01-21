6c31543d9f73dfcc9ff7c2fdecd5604a3556711d
Zachary Cliff <zc361@cam.ac.uk>
2023-01-21 15:27:11 +0000

    Main task complete for task 1D, will commit again with tests and demo program.

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -6,4 +6,22 @@ geographical data.
 
 """
 
-from .utils import sorted_by_key  # noqa
+from utils import sorted_by_key  # noqa
+#edited above line to "uitls" from ".utils"
+
+
+def rivers_with_station(stations):
+    '''Takes a list of monitoring stations, and returns a set of the rivers
+    monitored by those stations'''
+    return set([i.river for i in stations])
+    
+def stations_by_river(stations):
+    '''Takes a list of monitoring stations, and returns a dictionary mapping river names
+    to their monitoring stations'''
+    retval = {}
+    for i in stations:
+        if i.river in retval.keys:
+            retval[i.river].append(i)
+        else:
+            retval[i.river] = [i]
+    return retval
