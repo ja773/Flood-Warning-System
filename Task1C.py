bfa471301a37e3feb9e641a62feffa18384a94b3
ja773 <123214834+ja773@users.noreply.github.com>
2023-02-06 23:58:39 +0000

    Task1C

--- Task1C.py
+++ Task1C.py
@@ -1,13 +1,18 @@
 from floodsystem.geo import stations_within_radius
 from floodsystem.stationdata import build_station_list
+from floodsystem.utils import sorted_by_key
 
 def run():
     ''' Requirements for Task 1C '''
     cambridge = (52.2053, 0.1218)
     stations = build_station_list()
     cambridgestations = stations_within_radius(stations, cambridge, 10)
-    print(cambridgestations)
-    return cambridgestations
+    stats = []
+    for stat in cambridgestations:
+        stats.append(stat.name)
+    stats = sorted_by_key(stats, 0)
+    print(stats)
+    return stats
 
 if __name__ == "__main__":
     print("*** Task 1A: CUED Part IA Flood Warning System ***")
