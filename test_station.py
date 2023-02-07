e4c5509a2cdd13f1dc0b9fd4018aab6f36ed9fe1
ja773 <123214834+ja773@users.noreply.github.com>
2023-02-07 00:34:43 +0000

    Reupdated test_station.py

--- test_station.py
+++ test_station.py
@@ -10,15 +10,7 @@ from floodsystem.station import inconsistent_typical_range_stations
 def test_create_monitoring_station():
 
     # Create a station
-    s_id = "test-s-id"
-    m_id = "test-m-id"
-    label = "some station"
-    coord = (-2.0, 4.0)
-    trange = (-2.3, 3.4445)
-    river = "River X"
-    town = "My Town"
-    s1 = MonitoringStation(s_id, m_id, label, coord, trange, river, town)
-
+   
     s_id = "test-s-id"
     m_id = "test-m-id"
     label = "some station"
@@ -37,11 +29,20 @@ def test_create_monitoring_station():
     town = "My Town"
     s3 = MonitoringStation(s_id, m_id, label, coord, trange, river, town)
 
-    assert s.station_id == s_id
-    assert s.measure_id == m_id
-    assert s.name == label
-    assert s.coord == coord
-    assert s.typical_range == trange
-    assert s.river == river
-    assert s.town == town
+    s_id = "test-s-id"
+    m_id = "test-m-id"
+    label = "some station"
+    coord = (-2.0, 4.0)
+    trange = (-2.3, 3.4445)
+    river = "River X"
+    town = "My Town"
+    s1 = MonitoringStation(s_id, m_id, label, coord, trange, river, town)
+
+    assert s1.station_id == s_id
+    assert s1.measure_id == m_id
+    assert s1.name == label
+    assert s1.coord == coord
+    assert s1.typical_range == trange
+    assert s1.river == river
+    assert s1.town == town
     assert inconsistent_typical_range_stations([s1, s2, s3]) == [s2, s3]
